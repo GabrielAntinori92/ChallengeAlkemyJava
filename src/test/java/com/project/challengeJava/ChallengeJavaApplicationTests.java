ab4c132d61232ffbf9c773aa8a20de7cb9dd0122
GabrielAntinori92 <luisgantinori_1992@hotmail.com>
2021-10-07 07:59:53 -0300

    first commit

---
+++ src/test/java/com/project/challengeJava/ChallengeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.challengeJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
